21bb895fe0d6b417d90889a3f94f75747060ab26
ConnorRowe <connormagson@hotmail.co.uk>
2021-02-19 18:02:55 +0000

    Added variable roll commands.
    So users can choose min and max values with command arguments.

--- src/main/kotlin/net/fabricmc/roll/RollCommand.kt
+++ src/main/kotlin/net/fabricmc/roll/RollCommand.kt
@@ -2,6 +2,7 @@ package net.fabricmc.roll
 
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.IntegerArgumentType
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.network.MessageType
 import net.minecraft.server.command.CommandManager
@@ -16,17 +17,52 @@ object RollCommand : Command<Any?> {
     }
 
     fun register(dispatcher: CommandDispatcher<ServerCommandSource?>) {
+        // Fast roll 0 - 100
         dispatcher.register(CommandManager.literal("roll").executes(this::roll))
+
+        // Roll 0 - max var
+        dispatcher.register(
+            CommandManager.literal("roll").then(
+                CommandManager.argument("max", IntegerArgumentType.integer())
+                    .executes { ctx: CommandContext<ServerCommandSource?> ->
+                        rollRange(
+                            ctx,
+                            0,
+                            IntegerArgumentType.getInteger(ctx, "max")
+                        )
+                    })
+        )
+
+        // Roll min var - max var
+        dispatcher.register(
+            CommandManager.literal("roll").then(
+                CommandManager.argument("min", IntegerArgumentType.integer())
+                    .then(
+                        CommandManager.argument("max", IntegerArgumentType.integer())
+                            .executes { ctx: CommandContext<ServerCommandSource?> ->
+                                rollRange(
+                                    ctx,
+                                    IntegerArgumentType.getInteger(ctx, "min"),
+                                    IntegerArgumentType.getInteger(ctx, "max")
+                                )
+                            })
+            )
+        )
     }
 
     private fun roll(ctx: CommandContext<ServerCommandSource?>): Int {
+        return rollRange(ctx, 0, 100)
+    }
+
+    private fun rollRange(ctx: CommandContext<ServerCommandSource?>, min: Int, max: Int): Int {
         if (ctx.source?.entity is ServerPlayerEntity) {
             // Gets command source
             val serverCmdSrc: ServerCommandSource = ctx.source as ServerCommandSource
             // Gets player
             val player: ServerPlayerEntity = serverCmdSrc.entity as ServerPlayerEntity
             // Rolls the dice
-            val rollString = Random.nextInt(0, 100).toString() + "  ~  (0 - 100)"
+            val rollString =
+                Random.nextInt(min, max + 1).toString() + "  ~  (" + min.toString() + " - " + max.toString() + ")"
             // Builds the message
             val msg = LiteralText(player.displayName.asString() + " rolls " + rollString)
             // Gets iterator for all players
